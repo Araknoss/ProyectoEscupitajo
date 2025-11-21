15bd09655a94fe10d2d8a38972498e046e75b446
Araknoss <alvarocolladogarcia1@gmail.com>
2025-11-21 14:15:15 +0100

    ChunkPoolerUpdate

--- Assets/Scripts/Pooler.cs
+++ Assets/Scripts/Pooler.cs
@@ -4,28 +4,48 @@ using UnityEngine;
 
 public class Pooler : MonoBehaviour
 {
-    [SerializeField] private GameObject pooledObject;
-    [SerializeField] private int poolAmount = 20;
+    [SerializeField] private List<GameObject> pooledObjects = new List<GameObject>();
+    //[SerializeField] private int poolAmount = 20;
     [SerializeField] private List<GameObject> poolList = new List<GameObject>();
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        for(int i = 0; i < poolAmount; i++)
-        {
-            GameObject obj = Instantiate(pooledObject,gameObject.transform);
+        CreatePool();
+        
+    }
+
+    private void CreatePool()
+    {
+        for(int i = 0; i < pooledObjects.Count; i++)
+        {            
+            GameObject obj = Instantiate(pooledObjects[i],gameObject.transform);
+            obj.SetActive(false);
             poolList.Add(obj);
-            obj.SetActive(false); //obvejota
         }
     }
 
-    public GameObject GetPooledObject()
+    public GameObject GetPooledObject() //Devuelve el primer objeto inactivo del pool y lo activa
     {
-        for(int i = 0; i < poolAmount; i++)
+        for(int i = 0; i < pooledObjects.Count; i++)
         {
             if (!poolList[i].activeInHierarchy)
             {
                 poolList[i].SetActive(true);
                 return poolList[i];
+            }
+        }
+        return null;
+    }
+
+    public GameObject GetRandomPooledObject() //Busca un objeto aleatorio del pool que este inactivo y lo activa
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            int randomIndex = Random.Range(0, pooledObjects.Count);
+            if (!poolList[randomIndex].activeInHierarchy)
+            {
+                poolList[randomIndex].SetActive(true);
+                return poolList[randomIndex];
             }
         }
         return null;
