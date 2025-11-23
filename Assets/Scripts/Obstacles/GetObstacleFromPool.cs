01ac691f5922c3daf4a962b8093cbf10c4d2172d
Araknoss <alvarocolladogarcia1@gmail.com>
2025-11-23 12:38:21 +0100

    DynamicObstaclesUpdate

---
+++ Assets/Scripts/Obstacles/GetObstacleFromPool.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetObstacleFromPool : MonoBehaviour
+{
+    [SerializeField] private Pooler _pooler;
+    [SerializeField] private Transform initialTransform;
+    [SerializeField] private float bulletForce;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GameObject pooledObject = _pooler?.GetPooledObject();
+            if (pooledObject != null)
+            {
+                pooledObject.transform.position = initialTransform.position;
+                pooledObject.GetComponent<Rigidbody>().AddForce(Vector3.up * bulletForce, ForceMode.Impulse);
+                
+            }
+            
+        }
+    }
+}
