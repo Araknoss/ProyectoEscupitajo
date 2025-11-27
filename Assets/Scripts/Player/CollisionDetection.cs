dda6d14cc82c398765943d9ee9c1a949ddce1e3e
Araknoss <alvarocolladogarcia1@gmail.com>
2025-11-27 11:30:44 +0100

    Invulnerability&MouseInput

--- Assets/Scripts/Player/CollisionDetection.cs
+++ Assets/Scripts/Player/CollisionDetection.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollisionDetection : MonoBehaviour
 {
     public GameEvent onPlayerDeath;
     public GameEvent onWallDetection;
+
+    private bool isInvulnerable = false;
+    [SerializeField] private Toggle toggle;
     private void OnCollisionEnter2D(Collision2D collision)
     {        
         if (collision.gameObject.CompareTag("Wall"))
@@ -25,15 +29,18 @@ public class CollisionDetection : MonoBehaviour
         }
     }
 
-
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Obstacle"))
+        if (collision.gameObject.CompareTag("Obstacle") && !isInvulnerable)
         {
             onPlayerDeath.Raise(this, collision);
             Debug.Log("Player triggered with Obstacle");
         }        
+    }
+
+    public void SetInvulnerability()
+    {
+        isInvulnerable = toggle.isOn;
     }
 }
 
