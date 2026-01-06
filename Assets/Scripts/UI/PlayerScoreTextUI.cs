8878a9e37a8348df01a776de7d2bedde801f9802
Araknoss <alvarocolladogarcia1@gmail.com>
2026-01-06 14:05:24 +0100

    PlayerScoreUpdate
    He comentado DoTween porque daba errores. Mas adelante probaré LeanTween, de momento la animacion esta hecha con Lerp.

--- Assets/Scripts/UI/PlayerScoreTextUI.cs
+++ Assets/Scripts/UI/PlayerScoreTextUI.cs
@@ -11,7 +11,7 @@ public class PlayerScoreTextUI : MonoBehaviour
     [SerializeField] private float stayDuration = 1f;
     [SerializeField] private float hideDuration = 0.2f;
     [SerializeField] private float returnAnimationSpeed = 5f;
-
+    private float stayTimer=0;
     private float temporaryScore = 0f;
 
     [Header("References")]    
@@ -22,6 +22,17 @@ public class PlayerScoreTextUI : MonoBehaviour
     private void Update()
     {
         text.transform.localScale = Vector3.Lerp(text.transform.localScale, Vector3.one, Time.deltaTime * returnAnimationSpeed);
+
+        stayTimer+= Time.deltaTime;
+
+        if (stayTimer > stayDuration)
+        {
+            text.alpha = Mathf.Lerp(text.alpha, 0f, Time.deltaTime * 20);
+        }
+        if(stayTimer>=stayDuration+hideDuration)
+        {
+            EndAnimation();
+        }
     }
     public void HandleTrickPerformed(Component sender, object data)
     {
@@ -30,37 +41,41 @@ public class PlayerScoreTextUI : MonoBehaviour
             Trick trick = (Trick)data;
             AddTemporaryScore(trick.baseScore);
 
-            currentTween?.Kill();
-            PlayAnimation();
+            //currentTween?.Kill();
+            //PlayAnimation();
         }
     }
-    private void PlayAnimation()
-    {       
-        //Reset state
-        currentTween?.Kill();
+    //private void PlayAnimation()
+    //{       
+    //    //Reset state
+    //    currentTween?.Kill();
 
-        text.gameObject.SetActive(true);        
-        canvasGroup.alpha = 1f;
-        text.transform.localScale = Vector3.one * 1.3f;        
+    //    text.gameObject.SetActive(true);        
+    //    canvasGroup.alpha = 1f;
+    //    text.transform.localScale = Vector3.one * 1.3f;        
 
-        Sequence seq = DOTween.Sequence();
+    //    Sequence seq = DOTween.Sequence();
 
-        seq.AppendInterval(stayDuration)
-           .Append(canvasGroup.DOFade(0f, hideDuration))
-           .OnComplete(() => EndAnimation());
+    //    seq.AppendInterval(stayDuration)
+    //       .Append(canvasGroup.DOFade(0f, hideDuration))
+    //       .OnComplete(() => EndAnimation());
 
-        currentTween = seq;
-    }
+    //    currentTween = seq;
+    //}
 
     private void EndAnimation()
-    {
+    {        
+        text.alpha = 0f;
+        temporaryScore = 0f;        
         text.gameObject.SetActive(false);
-        temporaryScore = 0f;
-        currentTween?.Kill();
     }
 
     private void AddTemporaryScore(int score)
     {
+        text.transform.localScale = Vector3.one * 1.3f;
+        stayTimer = 0f;
+        text.gameObject.SetActive(true);
+        text.alpha = 1f;
         temporaryScore += score;
         text.text = "+" + temporaryScore.ToString();
     }
